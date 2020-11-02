070b37df687d1e91641585ebf42dcd6825077381
Elard Burga Goicochea <elard3d@hotmail.com>
2020-11-02 15:14:51 -0500

    inicial

---
+++ Proyect_YEService/src/main/java/com/example/demo/ProyectYeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectYeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectYeServiceApplication.class, args);
+	}
+
+}
